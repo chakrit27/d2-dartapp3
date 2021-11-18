52b583f8f9112be83b1887f51f1df9d494ac600e
chakrit27 <chakrit.indy2001@gmail.com>
2021-11-18 18:35:13 +0700

    Update v3

---
+++ bin/dartapp310.dart
@@ -0,0 +1,37 @@
+void main(){
+  print("Example od RElational operators");
+
+  int a = 10;
+  int b = 5;
+
+  bool res;
+
+  res = a > b;
+  print(res);
+  
+  res = a < b;
+  print(res);
+
+  res = a >= b;
+  print(res);
+
+  res = a <= b;
+  print(res);
+
+  res = a != b;
+  print(res);
+
+  res = a == b;
+  print(res);
+  
+  int m = 10;
+  String n = '10';
+
+  bool res2;
+
+  res2 = m == n;
+  print(res2);
+
+  res2 = m != n;
+  print(res2);
+}
